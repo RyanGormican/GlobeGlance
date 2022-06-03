71c329fb14164c6df3b95a7953b3462e6438e517
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-03 02:00:40 -0600

    Update and rename Date.js to Time.js

---
+++ src/components/Time.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { Context } from "../App";
+function Time() {
+ const { search, time, setTime, timezone, setTimeZone} = useContext(Context);
+
+  return (
+    
+   <div className="searching">
+    
+</div>
+
+
+
+);
+}
+
+export default Search;
