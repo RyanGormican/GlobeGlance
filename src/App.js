e8c5e6759b82e904c001603878bad860b3427296
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-03 01:23:44 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Date from "./components/Date";
+import Weather from "./components/Weather";
 const api = {
   key:"7bc27f1250aecc83d9e85aa10edc9203"
   base: "https://api.openweathermap.org/data/2.5/"
 }
+
 function App() {
   return (
     <div className="App">
