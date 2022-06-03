c57d70cca46072dd39676cf795507628c55def21
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-03 01:56:36 -0600

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../App";
-import { getDate } from "./Date.js";
-import { getTemp } from "./Weather.js";
 function Search() {
  const { search, setSearch} = useContext(Context);
 
   return (
-    
    <div className="searching">
    <input type = "text" className="search-tag" onChange={ e => setSearch.value)}
     value ={search}
     }
     
-
-
 </div>
-
-
-
 );
 }
 
