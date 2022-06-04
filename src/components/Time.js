4509a3dd6a3c1882bc7955539c8be2e20c04fcc3
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-04 01:37:06 -0600

    Update Time.js

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,19 +1,19 @@
 import React, {useCallback, useContext, useEffect } from "react";
 import { Context } from "../App";
 function Time() {
- const { search, time, setTime, timezone, setTimeZone} = useContext(Context)
-const updateTime = useCallback((event) => {
+ const { search, time, setTime, timezone, setTimeZone} = useContext(Context);
+const updateTime = useCallback(() => {
    var xmlHttp = new XMLHttpRequest();
    var searching = {search};
     xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
          callback(xmlHttp.responseText);
-    }
+    };
     xmlHttp.open("GET", "https://timezone.abstractapi.com/v1/current_time/?api_key=e4ff1ca165584a108b275349a35255a8&location=" + searching, true); // true for asynchronous
     xmlHttp.send(null);
  setTime(xmlHttp.datetime);
  setTimeZone(xmlHttp.timezone_abbreviation);
-};
+});
   useEffect(() => {
       document.addEventListener("onChange",updateTime);
       return ( ) => { 
@@ -23,8 +23,7 @@ const updateTime = useCallback((event) => {
   return (
     
    <div className="time">
-  document.write( { time } );
- document.write( { timeZone });
+  document.getElementById('time').innerHTML = { time } + { timeZone };
 </div>
 );
 }
