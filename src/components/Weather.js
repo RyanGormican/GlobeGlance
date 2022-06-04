1567f93415b4449ce9e68751ec86140fe99ba2eb
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-04 00:41:28 -0600

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,1 +1,26 @@
+import React, { useContext } from "react";
+import { Context } from "../App";
+function Weather() {
+ const { search, temp, setTemp} = useContext(Context);
+const updateTemp = useCallback((event) => {
+
+})
+  useEffect(() => {
+      document.addEventListener("onChange",updateTemp)
+      return ( ) => { 
+         document.removeEventListener("onChange",updateTemp)
+      };
+   }, [updateTemp]);
+  return (
+    
+   <div className="temp">
+   { temp } 
+</div>
+
+
+
+);
+}
+
+export default Weather;
 
