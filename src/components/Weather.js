99db26ae8a456bfb328fa3759df57ffb462b8707
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-04 00:51:48 -0600

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,7 @@ import { Context } from "../App";
 function Weather() {
  const { search, temp, setTemp} = useContext(Context);
 const updateTemp = useCallback((event) => {
- fetch(`${https://api.openweathermap.org/data/2.5/}weather?q=${search}&units=metric&APPID=${7bc27f1250aecc83d9e85aa10edc9203}`)
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=7bc27f1250aecc83d9e85aa10edc9203`)
         .then(res => res.json())
         .then(result => {
          setTemp(result);
