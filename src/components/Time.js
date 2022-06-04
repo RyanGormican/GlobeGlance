906ef4663c3f712f751328928f2e92b00b74d0e8
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-04 00:38:05 -0600

    Update Time.js

--- src/components/Time.js
+++ src/components/Time.js
@@ -3,7 +3,16 @@ import { Context } from "../App";
 function Time() {
  const { search, time, setTime, timezone, setTimeZone} = useContext(Context);
 const updateTime = useCallback((event) => {
- 
+   var xmlHttp = new XMLHttpRequest();
+   var searching = {search};
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+        callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", "https://timezone.abstractapi.com/v1/current_time/?api_key=e4ff1ca165584a108b275349a35255a8&location=" + searching, true); // true for asynchronous
+    xmlHttp.send(null);
+ setTime(xmlHttp.datetime);
+ setTimeZone(xmlHttp.timezone_abbreviation);
 })
   useEffect(() => {
       document.addEventListener("onChange",updateTime)
