48918d7a330f8c836cfe3dfb4efc3b88b503d1b8
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-05 02:11:44 -0600

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,15 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../App";
 function Search() {
  const { search, setSearch} = useContext(Context);
-function change(e){
-setSearch(e.target.value);
+function change(){
  var searcht = { search };
  console.log( searcht);
 }
   return (
    <div>
-   <input type = "text" className="search-tag" onChange={"change(e)" }
-    value ={search} />
+   <input type = "text" className="search-tag" onChange={e => setSearch(e.target.value)} }
+    value ={search} onKeyPress="change()" />
     
 </div>
 );
