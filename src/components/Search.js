f6c7143e5dd3813152d84f3badec8897598ac4a5
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-05 03:21:45 -0600

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
 import { Context } from "../App";
 function Search() {
- const { search, setSearch} = useContext(Context);
+ const { search, time, timezone, temp, setSearch} = useContext(Context);
 function change(){
  var searcht = { search };
+ var timet = { time };
+ var timezonet = { timezone };
+  var tempt = { temp };
  console.log( searcht);
+  console.log( timet);
+  console.log( timezonet);
+  console.log( tempt);
 }
   return (
    <div>
