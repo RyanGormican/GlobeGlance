93e900fe402ac674cfc05a50f59eb30e62f0626d
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-05 23:07:42 -0600

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,20 +3,21 @@ import { Context } from "../App";
 function Weather() {
  const { search, temp, setTemp} = useContext(Context);
 const updateTemp = useCallback((event) => {
+  if (event.key === "Enter") { 
  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=7bc27f1250aecc83d9e85aa10edc9203`)
         .then(res => res.json())
         .then(result => {
          setTemp(result);
      document.getElementById('temp').innerHTML = { temp }
         });
- 
+  }
 })
   useEffect(() => {
-   var searcht = document.getElementsByClassName("search-tag");
+   var searcht = document.getElementById("search-tag");
    if(searcht){
-         searcht.addEventListener("onfocus",updateTemp());
+         searcht.addEventListener("onKeyPress",updateTemp());
       return ( ) => { 
-          searcht.removeEventListener("onfocus",updateTemp())
+          searcht.removeEventListener("onKeyPress",updateTemp())
       };
    }
    }, [updateTemp]);
