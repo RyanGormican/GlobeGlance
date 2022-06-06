6207213cb32fca9d109fd4e0e0cf2635d8cb27b2
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-05 23:08:30 -0600

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -14,7 +14,7 @@ function change(){
 }
   return (
    <div>
-   <input type = "text" className="search-tag" onChange={e => setSearch(e.target.value)} 
+   <input type = "text" className="search-tag" id="search-tag" onChange={e => setSearch(e.target.value)} 
     value ={search} onKeyPress={change()} />
     
 </div>
