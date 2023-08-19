3f027df5c9bfb61f14379c625444c0c411a65e6f
RyanGormican <ryan.gormican@gmail.com>
2023-08-18 23:22:08 -0600

    a
    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import { Icon } from '@iconify/react';
 export default function Home() {
 return (
 	<div className= "Home">
+	 <div className="links">
+        <a href="https://www.linkedin.com/in/ryangormican/">
+          <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+        </a>
+        <a href="https://github.com/RyanGormican/CardCache">
+          <Icon icon="mdi:github" color="#e8eaea" width="60" />
+        </a>
+        <a href="https://ryangormicanportfoliohub.vercel.app/">
+          <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+        </a>
+      </div>
 		<div className= "Title">
+		<Icon icon="mdi:globe" />
 		GlobeGlance
+		<Icon icon="mdi:globe" />
+		<div>
+		<input type ="text" />
 		</div>
+		</div>
+		
 	</div>
 )
 
