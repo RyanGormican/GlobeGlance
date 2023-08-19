1bb4adf6a5bc04e2c9e4c78f69f12eb935e540d0
RyanGormican <ryan.gormican@gmail.com>
2023-08-18 23:13:45 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+
 function App() {
   return (
     <div className="App">
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Dashboard />} />
-    </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
