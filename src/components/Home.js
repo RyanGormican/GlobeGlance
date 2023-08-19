5d366728898c658856478dee67ddd63162d46658
RyanGormican <ryan.gormican@gmail.com>
2023-08-19 01:33:05 -0600

    a
    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,74 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import { Icon } from '@iconify/react';
-import { useNavigate } from 'react-router-dom';
-import Autosuggest from 'react-autosuggest';
-import debounce from 'lodash.debounce';
-
+import {useNavigate} from 'react-router-dom';
 export default function Home() {
-  const [search, setSearch] = useState('');
-  const [suggestions, setSuggestions] = useState(['New York']);
-  const navigate = useNavigate();
-
-  const sendSearch = () => {
-    if (!search) {
-      return;
-    } else {
-      navigate(`/dashboard/${search}`);
-    }
-  };
-
-  const defaultSuggestions = ['New York', 'Los Angeles', 'London', 'Paris', 'Tokyo'];
-
-  const getSuggestions = async (value) => {
-    const namePrefix = encodeURIComponent(value);
-    const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${namePrefix}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'a8d8ec1be0msh966006cb3a2cc91p117112jsne0110c5c10f0',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-
-      const suggestions = data.data.map((city) => city.city);
-      return suggestions;
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
-      return [];
-    }
-  };
-
-  const debouncedGetSuggestions = debounce(getSuggestions, 300);
-
-  const onSuggestionsFetchRequested = async ({ value }) => {
-    const suggestions = await debouncedGetSuggestions(value);
-    setSuggestions(suggestions);
-  };
-
-  const onSuggestionsClearRequested = () => {
-    setSuggestions([]);
-  };
-
-  const getSuggestionValue = (suggestion) => suggestion;
-
-  const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
-
-  const onSuggestionSelected = (event, { suggestion }) => {
-    navigate(`/dashboard/${suggestion}`);
-  };
-
-  const inputProps = {
-    value: search,
-    onChange: (event, { newValue }) => setSearch(newValue),
-  };
-
-  return (
-    <div className="Home">
-      <div className="links">
+const [search, setSearch] = useState('');
+const navigate = useNavigate();
+const sendSearch = () => {
+	if (!search)
+	{
+		return;
+	}else{
+		navigate(`/dashboard/${search}`)
+	}
+}
+return (
+	<div className= "Home">
+	 <div className="links">
         <a href="https://www.linkedin.com/in/ryangormican/">
           <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
         </a>
@@ -79,22 +25,19 @@ export default function Home() {
           <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
         </a>
       </div>
-      <div className="Title">
-        <Icon icon="mdi:globe" />
-        GlobeGlance
-        <Icon icon="mdi:globe" />
-        <div>
-          <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={onSuggestionsClearRequested}
-            getSuggestionValue={getSuggestionValue}
-            renderSuggestion={renderSuggestion}
-            onSuggestionSelected={onSuggestionSelected}
-            inputProps={inputProps}
-          />
-        </div>
-      </div>
-    </div>
-  );
+		<div className= "Title">
+		<Icon icon="mdi:globe" />
+		GlobeGlance
+		<Icon icon="mdi:globe" />
+		<div>
+		<input type ="text"   onChange={(event) => setSearch(event.target.value)}/>
+		<button onClick={sendSearch} > Search </button >
+		</div>
+		</div>
+		
+	</div>
+)
+
+
+
 }
