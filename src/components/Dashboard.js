cc0e76941aad3e02345d208d4c58a9e4735eee26
RyanGormican <ryan.gormican@gmail.com>
2023-08-19 00:50:09 -0600

    a
    a

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,17 +7,34 @@ const params = useParams();
 const [temperature, setTemperature] = useState('');
 const navigate = useNavigate();
 const search = params.search;
-const [theLat, setTheLat] = useState('');
-const [theLon, setTheLon] = useState('');
+const [theLat, setTheLat] = useState(0);
+const [theLon, setTheLon] = useState(0);
+const [windspeed,setWindSpeed] = useState(0);
+const [weather, setWeather] = useState('');
 const grabWeather = async () => {
     const coords = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q={search}&limit=1&appid=7bc27f1250aecc83d9e85aa10edc9203`);
     const coordinates = await coords.json();
     const { lat, lon} = coordinates[0];
+    console.log(coordinates);
     setTheLat(lat);
     setTheLon(lon);
-    const weather = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,daily&appid=7bc27f1250aecc83d9e85aa10edc9203`);
+    const weather = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${theLat}&lon=${theLon}&appid=7bc27f1250aecc83d9e85aa10edc9203`);
     const weatherData = await weather.json();
-    console.log(weatherData);
+    if (weatherData.cod === '200') {
+        // Extract relevant weather information
+        const currentWeather = weatherData.list[0];
+        const weatherDescription = currentWeather.weather[0].description;
+        const windSpeed = currentWeather.wind.speed;
+        const temperature = currentWeather.main.temp;
+
+        // Update the state with the extracted data
+        setTemperature(temperature);
+        setWeather(weatherDescription);
+        setWindSpeed(windSpeed);
+        console.log(weatherData);
+      } else {
+        console.log('Error fetching weather data:', weatherData.message);
+      }
 };
 const goHome = () => {
 		navigate('/')
@@ -36,6 +53,9 @@ return (
          <Row justify="center">
           <Col span={24}>
             <h1>{search}</h1>
+          </Col>
+           <Col span={24}>
+            <h1>Geographical</h1>
           </Col>
         </Row>
            <Row gutter={[16, 16]}>
@@ -63,13 +83,19 @@ return (
           </Col>
         </Row>
         <Row gutter={[16, 16]} justify="center">
-          <Col span={20}>
-         
+          <Col span={12}>
+            Weather:
+          </Col>
+            <Col span={12}>
+                 {weather}
           </Col>
         </Row>
         <Row gutter={[16, 16]} justify="center">
-          <Col span={20}>
-        
+            <Col span={12}>
+            Wind Speed
+          </Col>
+            <Col span={12}>
+                 {windspeed}
           </Col>
         </Row>
         <Row gutter={[16, 16]} justify="center">
